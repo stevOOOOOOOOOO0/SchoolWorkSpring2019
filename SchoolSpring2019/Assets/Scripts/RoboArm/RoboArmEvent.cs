eb575974f219889e1b6bfbc92958b91045279581
Stevo <stevOOOOOOOOOO0@yahoo.com>
2019-01-30 11:19:29 -0700

    Sprite work and a bit of coding in unity

---
+++ SchoolSpring2019/Assets/Scripts/RoboArm/RoboArmEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RoboArmEvent : MonoBehaviour
+{
+
+	public Rigidbody2D RoboArm;
+	public UnityEvent Event;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		RoboArm = GetComponent<Rigidbody2D>();
+	}
+
+	void OncollisionEnter2D()
+	{
+		Event.Invoke();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
