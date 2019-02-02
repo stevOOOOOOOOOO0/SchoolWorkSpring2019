bf128ef0af473963dde863a2723928f89bc809a7
Laptop <StevOOOOOOOOO0@YAHOO.com>
2019-02-01 19:00:17 -0700

    movement

---
+++ SchoolSpring2019/Assets/Scripts/RoboArm/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	private CharacterController _armPot
+	private float _translation = 0f;
+	private float _verticle = 0f;
+	private float _translationSpeed = 10f;
+	private Vector3 _moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _armPot = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	if (Input.GetButtonDown("Space"))
+	{
+		_verticle = 10f;
+	}
+	_moveDirection = new Vector3(Input.GetAxis("Horizontal") * _translationSpeed, _vertical, 0.0f);
+	_armpot.Move(_moveDirection * Time.deltaTime);
+    }
+}
