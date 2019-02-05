4358ea231f4a3bce0068ec519fc8d65172ba3122
Laptop <StevOOOOOOOOO0@YAHOO.com>
2019-02-05 12:17:18 -0700

    3 animal classes

---
+++ SchoolSpring2019/Assets/Scripts/Animal/YammaBaby.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YammaBaby : Yamma {
+
+	public float size;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
