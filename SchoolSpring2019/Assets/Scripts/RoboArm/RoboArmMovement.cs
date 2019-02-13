aa3de7e2de1d83c2ee0fbcfcb58f73a810a898ce
Stevo <stevOOOOOOOOOO0@yahoo.com>
2019-02-13 11:21:19 -0700

    added movement and animations for the movement

---
+++ SchoolSpring2019/Assets/Scripts/RoboArm/RoboArmMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboArmMovement : MonoBehaviour
+{
+
+	private Rigidbody2D _player;
+	public Animator Anims;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_player.AddForce(_player.transform.right * Input.GetAxis("Horizontal") * 50);
+		if (Input.GetAxis("Horizontal") > 0)
+			Anims.SetTrigger("MovementRight");
+		if (Input.GetAxis("Horizontal") < 0)
+			Anims.SetTrigger("MovementLeft");
+		if (Input.GetAxis("Horizontal") == 0f)
+		{
+			Anims.SetTrigger("NotMoving");
+			Anims.ResetTrigger("MovementLeft");
+			Anims.ResetTrigger("MovementRight");
+		}
+	}
+}
